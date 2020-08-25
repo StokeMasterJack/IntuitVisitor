4fc248b3ce3c38f5105206593b27c9f11a3edb87
Maximiliano Firtman <firtman@gmail.com>
2020-08-25 14:36:33 -0700

    First commmit; hello world!

---
+++ app/src/main/java/com/intuit/august2020/intuitvisitor/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.intuit.august2020.intuitvisitor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button_login.setOnClickListener {
+
+        }
+    }
+}
